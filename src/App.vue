816986af253edabdb3650f7fa857900011c14d69
susangithaiga <susangithaiga26@gmail.com>
2024-03-11 10:43:40 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BatteryCharger />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BatteryCharger from "./components/BatteryCharger.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BatteryCharger,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: #0b0c0d;
 }
 </style>
---
+++ src/components/BatteryCharger.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main-outer">
+    <h1>Battery charger Animation</h1>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script></script>
+<style scoped>
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 30px;
+  padding-bottom: 5%;
+}
+.box {
+  width: 150px;
+  height: 80px;
+  border: 3px solid whitesmoke;
+  border-radius: 12px;
+  position: relative;
+}
+.box::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -10px;
+  transform: translate(10px, -50%);
+  width: 10px;
+  height: 25px;
+  background-color: transparent;
+  border: 1px solid whitesmoke;
+}
+.box::after {
+  width: 0;
+  height: 100%;
+  content: "";
+  position: absolute;
+  background: #f1efef;
+  border-radius: 7px;
+  animation: animate 5s linear infinite;
+}
+@keyframes animate {
+  0% {
+    width: 0%;
+    background: red;
+  }
+  25% {
+    width: 25%;
+    background: orange;
+  }
+  50% {
+    width: 50%;
+    background: yellow;
+  }
+  75% {
+    width: 75%;
+    background: #d7fc03;
+  }
+  100% {
+    width: 100%;
+    background: green;
+  }
+}
+</style>
